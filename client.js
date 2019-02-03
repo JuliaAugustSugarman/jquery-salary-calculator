9e9636068401442b65b6fa11c5358b99110d0973
JuliaAugustSugarman <jules.august@gmail.com>
2019-02-03 16:27:40 -0600

    TaDaH

--- client.js
+++ client.js
@@ -6,16 +6,24 @@ function onReady() {
     console.log('jQuery is ready');
     $('#employeeSubmitButton').on('click', submitEmployeeInformation)
     console.log('employee Click Button Clicked');
-    $('.clearInputFields').val('');
+    $('#newEmployeeRow').on('click', '.deleteButton', deleteEmployee)
+    console.log('remove employee button');
+    
 let annualCosts = 0; 
 annualCosts = annualCosts + annualSalary;
 console.log('annualCosts equation');
   
 }//end onReady function
 
+function deleteEmployee(){
+    console.log('inside Delete Employee');
+    $(this).closest('tr').remove();
+    
+}//end of delete employee function
 
-let annualCosts = 0; //varialble deffinition for annualCosts within the submitEmployee
+let annualCosts = 0; 
 let monthlyCosts = 0;
+
 
 function submitEmployeeInformation() {
     console.log('Employee information submit function');
@@ -24,8 +32,9 @@ function submitEmployeeInformation() {
     let idNumber = $( '#idNumber').val();
     let jobTitle = $( '#jobTitle').val();
     let annualSalary = $( '#annualSalary').val();
+   
 $('#newEmployeeRow').append( `<tr><td>` 
-+firstName + `</td><td>` +lastName+ `</td><td>` +idNumber+ `</td><td>` +jobTitle+ `</td><td>` +annualSalary+ `</td></tr>`
++firstName + `</td><td>` +lastName+ `</td><td>` +idNumber+ `</td><td>` +jobTitle+ `</td><td>` +annualSalary+ `</td> <td><button class='deleteButton'>Delete</button></td></tr>`
 );//end submitEmployeeInformation
 
 $('.clearInputFields').val('');
@@ -33,7 +42,6 @@ $('.clearInputFields').val('');
 //annual costs equations
 annualCosts += Number(annualSalary);
 console.log('this is the annual cost', annualCosts);
-
 
 console.log('annualCosts equation');
 //monthly costs
@@ -43,8 +51,13 @@ console.log('monthly costs');
 $('#monthlyCalculator').text(monthlyCosts);
 if (monthlyCosts > 20000){
     console.log('Monthly Cost:', monthlyCosts);
+    $('#monthlyCalculator').css('background-color','red');//change color of text
+
+    
     
 }//end if statement
+
+
 
 
 
