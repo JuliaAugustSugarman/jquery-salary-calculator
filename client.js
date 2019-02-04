6f8bd392635ec849cc315719ac4ac4117508697f
JuliaAugustSugarman <jules.august@gmail.com>
2019-02-04 09:23:35 -0600

    fixed math

--- client.js
+++ client.js
@@ -8,58 +8,55 @@ function onReady() {
     console.log('employee Click Button Clicked');
     $('#newEmployeeRow').on('click', '.deleteButton', deleteEmployee)
     console.log('remove employee button');
-    
-let annualCosts = 0; 
-annualCosts = annualCosts + annualSalary;
-console.log('annualCosts equation');
-  
+
+    let annualCosts = 0;
+    annualCosts = annualCosts + annualSalary;
+    console.log('annualCosts equation');
+
 }//end onReady function
 
-function deleteEmployee(){
+function deleteEmployee() {
     console.log('inside Delete Employee');
     $(this).closest('tr').remove();
-    
+
 }//end of delete employee function
 
-let annualCosts = 0; 
+let annualCosts = 0;
 let monthlyCosts = 0;
 
 
-function submitEmployeeInformation() {
+function submitEmployeeInformation(){
     console.log('Employee information submit function');
-    let firstName = $( '#firstName' ).val();
-    let lastName =  $( '#lastName' ).val();
-    let idNumber = $( '#idNumber').val();
-    let jobTitle = $( '#jobTitle').val();
-    let annualSalary = $( '#annualSalary').val();
-   
-$('#newEmployeeRow').append( `<tr><td>` 
-+firstName + `</td><td>` +lastName+ `</td><td>` +idNumber+ `</td><td>` +jobTitle+ `</td><td>` +annualSalary+ `</td> <td><button class='deleteButton'>Delete</button></td></tr>`
-);//end submitEmployeeInformation
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $('#idNumber').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
 
-$('.clearInputFields').val('');
+    $('#newEmployeeRow').append(`<tr><td>`
+        + firstName + `</td><td>` + lastName + `</td><td>` + idNumber + `</td><td>` + jobTitle + `</td><td>` + annualSalary + `</td> <td><button class='deleteButton'>Delete Employee</button></td></tr>`
+    );
 
-//annual costs equations
-annualCosts += Number(annualSalary);
-console.log('this is the annual cost', annualCosts);
+    $('.clearInputFields').val('');
 
-console.log('annualCosts equation');
-//monthly costs
-monthlyCosts = Number(annualCosts/12);
-console.log('monthly costs');
+    //annual costs equations
+    annualCosts += Number(annualSalary);
+    console.log('this is the annual cost', annualCosts);
 
-$('#monthlyCalculator').text(monthlyCosts);
-if (monthlyCosts > 20000){
-    console.log('Monthly Cost:', monthlyCosts);
-    $('#monthlyCalculator').css('background-color','red');//change color of text
 
-    
-    
-}//end if statement
+    console.log('annualCosts equation');
+    //monthly costs
+    monthlyCosts = Number(annualCosts / 12);
+    console.log('monthly costs');
+
+    $('#monthlyCalculator').text(monthlyCosts);
+    if (monthlyCosts > 20000) {
+        console.log('Monthly Cost:', monthlyCosts);
+        $('#monthlyCalculator').css('background-color', 'red');//change color of text
 
 
 
-
+    }//end if statement
 
 
 }//end submitEmployee info Function
